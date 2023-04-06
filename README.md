d48bf696c1730469b145e831e18a4094df3d02c9
Yumicode <75518128+helene-nguyen@users.noreply.github.com>
2023-04-06 09:47:48 +0200

    :tada: start: first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Start with Ruby
+
+## Introduction
+
+Ruby is an open source programming language with a focus on simplicity and productivity.
+
+Here are some main features of Ruby :
+
+- Ruby is open-source and is freely available on the Web, but it is subject to a license
+- Ruby is a server-side scripting language similar to Python and PERL
+- Ruby is very much scalable
+- Big programs written in Ruby are easily maintainable
+- Ruby can be used for developing Internet and intranet applications
+- Ruby can be embedded into Hypertext Markup Language (HTML)
+
+You'll find here some examples of using Ruby for my personnal use and test.
+
+Hope it can help :)
+
+## Tools and versions
+
+- OS
+Kali Linux OS - Ruby v3.1.2
+Windows OS - Ruby v3.2.2
+
+- IDE
+VSCodium v1.77.1
+
+
+
+## Sources
+
+[Features found here](https://www.tutorialspoint.com/ruby/ruby_overview.htm#:~:text=Features%20of%20Ruby,similar%20to%20Python%20and%20PERL.)
---
+++ tuto.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+
+class MegaGreeter
+  attr_accessor :names
+
+  # Create an object
+  def initialize(names = "World")
+    @names = names
+  end
+
+  # Greet everybody
+  def say_hi
+    if @names.nil?
+      puts "Sorry, nobody is here..."
+    elsif @names.respond_to?("each")
+      # names is a list of names, let's make a loop
+      @names.each do |name|
+        puts "Hello #{name}!"
+      end
+    else
+      puts "Hello #{@names}!"
+    end
+  end
+
+  def say_bye
+    if @names.respond_to?("join")
+      puts "Goodbye #{@names.join(",")}. Come back soon !"
+    end
+  end
+end
+
+if __FILE__ == $0
+  megagreeter = MegaGreeter.new
+  megagreeter.say_hi
+
+  # change the name
+  megagreeter.names = "everybody"
+  megagreeter.say_hi
+
+  # give an array of names
+  megagreeter.names = ["Honey", "Lune", "Mymooh", "Fredo", "Gaetan"]
+  megagreeter.say_hi
+  megagreeter.say_bye
+
+  megagreeter.names = nil
+  megagreeter.say_hi
+end
